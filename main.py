ec0801c21f10619d526c808e243a47af0b385973
bryoh <brianbryo@gmail.com>
2016-03-04 21:37:23 +0000

    [PYTHON] Updated function calls which will change as time goes by.

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
-from flask import Flask, request,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/about")
+def portfolio_():
+    return render_template("about.html")
+
+
+@app.route("/highlights")
+def portfolio_():
+    return render_template("highlights.html")
+
+
 @app.route("/")
 def index():
-    return render_template("profile.html")
+    return render_template("homepage.html")
+
+
+@app.route("/latest")
+def index():
+    return render_template("latest.html")
+
+
+@app.route("/menu")
+def portfolio_():
+    return render_template("menupage.html")
+
+
+@app.route("/portfolio")
+def portfolio_():
+    return render_template("Portfolio.html")
+
+
+
+
+
+@app.route("/gallery")
+def portfolio_():
+    return render_template("Portfolio.html")
+
+
+
+
+
 
 '''
 @app.route("/b",method=["GET", "POST"])
